ec5e9803dd741447d0272289ea0a2cf1c75ce8ce
matth <matth@sjsoft.com>
2013-06-07 09:03:59 +0200

    On windows, use the binary distribution.

--- wardeninstaller/Installer.py
+++ wardeninstaller/Installer.py
@@ -8,20 +8,36 @@ def _run(command):
     subprocess.check_call(command)
 
 
-def install_libraries(home, run=None, pip=None, lib=None, scripts=None):
+def install_libraries(home, run=None, pip=None, lib=None, scripts=None, prefix=None):
     run = run or _run
     pip = pip or 'pip'
     if lib is None:
         lib= distutils.sysconfig.get_python_lib()
     if scripts is None:
         scripts=os.path.dirname(sys.executable)
+    if prefix is None:
+        prefix=os.path.dirname(scripts)
+
+    if 'win' in sys.platform:
+        pipping_easy_install = 'pipping_easy_install'
+
+        run([pipping_easy_install, 'Twisted==11.1.0'])
+        run([pipping_easy_install, 'psutil'])
+        run([pipping_easy_install, 'pycairo'])
+        run([pipping_easy_install, 'pywin32'])
+        run([pipping_easy_install, 'zope.interface'])
+    else:
+        run([pip, 'install', "http://cairographics.org/releases/py2cairo-1.8.10.tar.gz" ])
+
 
     run([pip, 'install', 'http://github.com/richg/whisper/tarball/0.9.x-warden#egg=whisper-0.9.10-warden',
          '--install-option=--install-scripts=%s' % scripts,
+         '--install-option=--prefix=%s' % prefix,
          '--install-option=--install-lib=%s' % lib,
          '--install-option=--install-data=%s' % (os.path.join(home, 'graphite'))])
     run([pip, 'install', 'http://github.com/richg/carbon/tarball/0.9.x-warden#egg=carbon-0.9.x-warden',
          '--install-option=--install-scripts=%s' % scripts,
+         '--install-option=--prefix=%s' % prefix,
          '--install-option=--install-lib=%s' % lib,
          '--install-option=--install-data=%s' % (os.path.join(home, 'graphite'))])
     run([pip, 'install', "http://github.com/richg/Diamond/tarball/master",
@@ -29,13 +45,13 @@ def install_libraries(home, run=None, pip=None, lib=None, scripts=None):
          '--install-option=--install-lib=%s' % lib,
          '--install-option=--install-data=%s' % (os.path.join(home, 'diamond'))])
     run([pip, 'install', "Django==1.4.3"])
-    run([pip, 'install', "http://github.com/richg/graphite-web/tarball/0.9.x#egg=graphite-web-0.9.x-warden",
+    run([pip, 'install', "http://github.com/richg/graphite-web/tarball/0.9.x-warden#egg=graphite-web-0.9.x-warden",
          '--install-option=--install-scripts=%s' % scripts,
+         '--install-option=--prefix=%s' % prefix,
          '--install-option=--install-lib=%s' % lib,
          '--install-option=--install-data=%s' % (os.path.join(home, 'graphite'))])
 
     run([pip, 'install', "CherryPy==3.2.2" ])
-    run([pip, 'install', "http://cairographics.org/releases/py2cairo-1.8.10.tar.gz" ])
 
     run([pip, 'install', "sentry==5.2.2"])
     run([pip, 'install', "http://github.com/richg/warden/tarball/master",
